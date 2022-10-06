fe5ac51239cdffe4b3ed0c2a16c04ccab61177ad
seongjin_song <seongjin_song@tmax.co.kr>
2022-10-06 18:55:16 +0900

    feat : 랜덤값을 확인하고 이동할지 말지에 대한 기능 구현

---
+++ src/main/java/racingcar/domain/Move.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+public class Move {
+
+    public static final int FORWARD_CRITERIA = 4;
+    private final int Min_NUMBER = 0;
+    private final int MAX_NUMBER = 9;
+    private boolean isMoved = false;
+
+    public void execute(int number) {
+        if (number < Min_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException("0이상 9이하의 값을 입력해 주세요");
+        }
+        if (number >= FORWARD_CRITERIA) {
+            move();
+        }
+    }
+
+    public boolean isMoved() {
+        return isMoved;
+    }
+
+    public void move() {
+        isMoved = true;
+    }
+}
