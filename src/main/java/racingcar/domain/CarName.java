09792555d3a043b4205c7ffd1b7aa84649290e0f
송성진 <seongjin_song@tmax.co.kr>
2022-10-10 15:49:01 +0900

    refactor : readme 수정

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -2,10 +2,11 @@ package racingcar.domain;
 
 public class CarName {
 
+    public static final int CAR_NAME_MAX_SIZE = 5;
     private final String name;
 
     public CarName(String name) {
-        if (name.length() > 5) {
+        if (name.length() > CAR_NAME_MAX_SIZE) {
             throw new IllegalArgumentException("차 이름을 5자 이하로 입력해 주세요.");
         }
         this.name = name;
